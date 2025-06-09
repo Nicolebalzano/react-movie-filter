9d7d4e39de62b6a5f62d09826ebfcd2f23fa9a4e
Nicolebalzano <nicole.balzano03@gmail.com>
2025-06-09 23:07:35 +0200

    feat:stile modificato

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="container mt-5">
-      <h1 className="text-center mb-5">Lista di Film</h1>
       <FilmList films={filmList} />
     </div>
   );
--- src/components/FilmList.jsx
+++ src/components/FilmList.jsx
@@ -18,12 +18,11 @@ const FilmList = ({ films }) => {
 
   return (
     <div className="container mt-5">
-      <h1 className="text-center mb-5">Scegli il tuo film</h1>
-
+      <h1 className="text-center mb-5 fw-bold">Booleanflix</h1>
       {/* Form di ricerca */}
       <form className="mb-4">
         <div className="d-flex">
-          <h5 className="col-3">Cerca il film:</h5>
+          <h5 className="col-3 mx-3 p-2">Cerca il film:</h5>
           <input type="text" placeholder="Cerca..." className="form-control" onChange={(event) => setSearchTerm(event.target.value)} />
         </div>
       </form>
@@ -35,7 +34,7 @@ const FilmList = ({ films }) => {
           onChange={(event) => setSelectedType(event.target.value)}
           className="form-select mt-3 w-50"
         >
-           <option value="Genere">Genere</option>
+           <option value="tutti">Tutti i generi</option>
           <option value="Fantascienza">Fantascienza</option>
           <option value="Thriller">Thriller</option>
           <option value="Romantico">Romantico</option>
@@ -44,14 +43,14 @@ const FilmList = ({ films }) => {
       </div>
 
       {/* Lista film */}
-      <ul className="list-unstyled d-flex flex-wrap p-5">
+      <ul className="list-unstyled d-flex flex-wrap p-5 justify-content-center">
         {filmFiltrati.map((curMovie, index) => (
           <li
             key={index}
-            className="mt-2 fs-3 col-6 text-center border border-secondary"
+            className="mt-2 fs-3 col-4 text-center border border-secondary m-4 pb-3 fw-bolder"
           >
             {curMovie.title} <br />
-            <span className="text-secondary fs-5 border border-secondary px-4 shadow">
+            <span className=" fs-5 border border-secondary px-4 shadow my-5">
               {curMovie.genre}
             </span>
           </li>
